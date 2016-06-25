3bff5088b5a9921ef96d11673a8372da8590ce9a
David Richfield <davidrichfield@gmail.com>
2016-06-25 22:05:07 +0000

    re-add edit function

--- parlitest.php
+++ parlitest.php
@@ -111,6 +111,10 @@ switch ( isset( $_GET['action'] ) ? $_GET['action'] : '' ) {
 		doUpload($_GET['uri'], $_GET['filename'], $_GET['pagecontent'], "Testing API upload", $_GET['ignore']);
 		break;
 
+	case 'edit':
+		doEdit();
+		break;
+
 	case 'identify':
 		doIdentify();
 		break;
@@ -402,7 +406,66 @@ function doApiQuery( $post, &$ch = null , $mode = '' ) {
 	return $ret ;
 }
 
+function doEdit() {
+	global $mwOAuthIW;
 
+	$ch = null;
+
+	// First fetch the username
+	$res = doApiQuery( array(
+		'format' => 'json',
+		'action' => 'query',
+		'meta' => 'userinfo',
+	), $ch );
+
+	if ( isset( $res->error->code ) && $res->error->code === 'mwoauth-invalid-authorization' ) {
+		// We're not authorized!
+		echo 'You haven\'t authorized this application yet! Go <a href="' . htmlspecialchars( $_SERVER['SCRIPT_NAME'] ) . '?action=authorize">here</a> to do that.';
+		echo '<hr>';
+		return;
+	}
+
+	if ( !isset( $res->query->userinfo ) ) {
+		header( "HTTP/1.1 $errorCode Internal Server Error" );
+		echo 'Bad API response: <pre>' . htmlspecialchars( var_export( $res, 1 ) ) . '</pre>';
+		exit(0);
+	}
+	if ( isset( $res->query->userinfo->anon ) ) {
+		header( "HTTP/1.1 $errorCode Internal Server Error" );
+		echo 'Not logged in. (How did that happen?)';
+		exit(0);
+	}
+	$page = 'User talk:' . $res->query->userinfo->name;
+
+	// Next fetch the edit token
+	$res = doApiQuery( array(
+		'format' => 'json',
+		'action' => 'tokens',
+		'type' => 'edit',
+	), $ch );
+	if ( !isset( $res->tokens->edittoken ) ) {
+		header( "HTTP/1.1 $errorCode Internal Server Error" );
+		echo 'Bad API response: <pre>' . htmlspecialchars( var_export( $res, 1 ) ) . '</pre>';
+		exit(0);
+	}
+	$token = $res->tokens->edittoken;
+
+	// Now perform the edit
+	$res = doApiQuery( array(
+		'format' => 'json',
+		'action' => 'edit',
+		'title' => $page,
+		'section' => 'new',
+		'sectiontitle' => 'Hello, world',
+		'text' => 'This message was posted using the OAuth Hello World application, and should be seen as coming from yourself. To revoke this application\'s access to your account, visit [[:' . $mwOAuthIW . ':Special:OAuthManageMyGrants]]. ~~~~',
+		'summary' => '/* Hello, world */ Hello from OAuth!',
+		'watchlist' => 'nochange',
+		'token' => $token,
+	), $ch );
+
+	echo 'API edit result: <pre>' . htmlspecialchars( var_export( $res, 1 ) ) . '</pre>';
+	echo '<hr>';
+}
 
 function doUpload ( $filetosend , $new_file_name , $desc , $comment , $ignorewarnings ) {
 
