8b608f83fef87b6223a3da1618e9b8b0427b8a41
David Richfield <davidrichfield@gmail.com>
2019-08-18 12:50:37 +0200

    Fix links and typo

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer" role="contentinfo">
   <div class="container">
-    <p>Built by <a href="https://github.com/slashme" target="_blank">David Richfiled</a>. Code licensed under <span><a href="https://raw.githubusercontent.com/slashme/parliamentdiagram/master/LICENSE.md" target="_blank">GPL v2</a></span>.</p>
+    <p>Built by <a href="https://en.wikipedia.org/wiki/User:Slashme" target="_blank">David Richfield</a>. Code licensed under <span><a href="https://raw.githubusercontent.com/slashme/parliamentdiagram/master/LICENSE.md" target="_blank">GPL v2</a></span>.</p>
     <ul class="base-footer-links muted">
       <li><a href="https://github.com/slashme/parliamentdiagram">Source repository</a></li>
       <li> · </li>
