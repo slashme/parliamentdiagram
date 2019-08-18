561c7b4e8d7cf2db18d1a6fa55b28ec9c17eae0d
David Richfield <davidrichfield@gmail.com>
2019-08-18 10:11:42 +0200

    try out old color code

--- parlitest.php
+++ parlitest.php
@@ -987,7 +987,8 @@ function addParty(newname="", newcolor=""){
         //Party color input control
         var input=document.createElement('div');
 	if(newcolor==""){newcolor=getRandomColor()}
-        input.innerHTML = '<input class="right color" type="text" name="Color' +  i + '" value= "' +  newcolor + '" >'
+        //input.innerHTML = '<input class="right color" type="text" name="Color' +  i + '" value= "' +  newcolor + '" >'
+        input.innerHTML = '<input class="right color" type="text" name="Color' +  i + '" value= "' +  getRandomColor() + '" >'
         newpartydiv.appendChild(input);
         //Party border checkbox name tag
         var partycolor=document.createElement('div');
