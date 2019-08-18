69e732f959a0bd332e61e842616d4004a2766cb0
David Richfield <davidrichfield@gmail.com>
2019-08-18 09:42:30 +0200

    move header and footer into separate files

--- parlitest.php
+++ parlitest.php
@@ -1,18 +1,5 @@
 <?php require('auth.php'); ?>
-<!DOCTYPE html>
-<html dir="ltr" lang="en">
-<head>
-<link rel="stylesheet" type="text/css" href="css/parliamentstyle.css">
-<link rel="stylesheet" href="css/select2.min.css" />
-<!--
-//document.write("\<script src='jquery.min.js' type='text/javascript'>\<\/script>"); //For local debugging
--->
-<script src='https://tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/3.2.1/jquery.min.js' type='text/javascript'></script>
-<script type="text/javascript" src="jscolor/jscolor.js"></script>
-<script src="js/select2.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
-</head>
-<body>
+<?php require('top.php'); ?>
 <div class=block id=header>
 <?php require('header.php'); ?>
 </div>
@@ -81,5 +68,4 @@ Please submit bug reports and feature requests at the project's <a href="https:/
     <br>
   </div>
 </div>
-</body>
-</html>
+<?php require('footer.php'); ?>
