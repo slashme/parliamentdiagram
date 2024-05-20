a77396eea19b8ecb8150eb67184b1e9f81e750c2
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-05-20 13:04:58 +0200

    Add Flask skeleton

---
+++ src/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+# redirects
+@app.route("/")
+@app.route("/index")
+@app.route("/index.php")
+def root():
+    return redirect(url_for("archinputform"))
+
+# legacy redirects
+# @app.route("/parliamentinputform")
+@app.route("/parliamentinputform.html")
+# @app.route("/parlitest")
+@app.route("/parlitest.php")
+@app.route("/archinputform.php")
+def archinputform_redirect():
+    return redirect(url_for("archinputform"))
+
+@app.route("/USinputform.html")
+@app.route("/USinputform.php")
+def usinputform_redirect():
+    return redirect(url_for("usinputform"))
+
+@app.route("/westminsterinputform.html")
+@app.route("/westminsterinputform.php")
+def westminsterinputform_redirect():
+    return redirect(url_for("westminsterinputform"))
+
+
+# main pages
+@app.route("/archinputform")
+def archinputform():
+    return ("Not yet implemented", 503)
+
+@app.route("/USinputform")
+def usinputform():
+    return ("Not yet implemented", 503)
+
+@app.route("/westminsterinputform")
+def westminsterinputform():
+    return ("Not yet implemented", 503)
+
+
+# direct requests
+@app.post("/newarch")
+@app.post("/newarch.py")
+def newarch_generation():
+    return ("Not yet implemented", 503)
+
+@app.post("/westminster")
+@app.post("/westminster.py")
+def westminster_generation():
+    return ("Not yet implemented", 503)
