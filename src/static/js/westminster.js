f68176a5c97f30a186c293e6339b545b58db276d
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-05-25 18:11:39 +0200

    Move JS around
    Only leave common function in main.js,
    don't import Arch JS code

--- src/static/js/westminster.js
+++ src/static/js/westminster.js
@@ -2,16 +2,6 @@ $(document).ready(function () {
     jscolor.installByClassName("jscolor");
 });
 
-//Generate random color, based on http://stackoverflow.com/questions/1484506
-function getRandomColor() {
-    const letters = '0123456789ABCDEF'.split('');
-    let color = ''; // In my case, I don't want the leading #
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 function CallDiagramScript() {
     // Create request payload
     let payload = {};
@@ -237,8 +227,4 @@ function addParty() {
     // Add a newline
     newpartydiv.appendChild(document.createElement("br"));
     jscolor.installByClassName("jscolor");
-}
-
-function deleteParty(i) {
-    document.getElementById("partylistcontainer").removeChild(document.getElementById("party" + i));
 }
