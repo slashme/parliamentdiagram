51711c768ada6594ee0dbeb1bfa9a1adaa6c60d7
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-05-27 02:42:36 +0200

    Fix again, and extract from the response if possible

--- src/static/js/arch.js
+++ src/static/js/arch.js
@@ -467,7 +467,11 @@ function postToUpload(fname, linkdata, legendtext, ignore = false) {
             successdiv.className = 'success';
             successdiv.append("Image successfully uploaded on ");
             const a = successdiv.appendChild(document.createElement("a"));
-            a.href = "commons.wikimedia.org/wiki/File:" + fname.replace(" ", "_");
+            try {
+                a.href = data.upload.imageinfo.descriptionurl;
+            } catch (e) {
+                a.href = "https://commons.wikimedia.org/wiki/File:" + fname.replace(" ", "_");
+            }
             a.append("Commons");
             successdiv.append(".");
             uploadbutton.parentElement.appendChild(successdiv);
