c7e04c4018d42796d557d2f9c31a827dcfc7443b
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-07-10 13:22:23 +0200

    Renovate the US implem

--- src/static/js/us.js
+++ src/static/js/us.js
@@ -1,48 +1,39 @@
 function CallArchScript() {
-    const requestJSON = {};  // This is what we send to the python script
-    requestJSON.denser_rows = false; //Because I'm not going to implement this here yet
-    requestJSON.parties = []
-    $("input").each(function () {
-        if (this.value < 1) { this.value = 0; }; //Make sure it's a number!
-        switch (this.name) {
-            case "Dem":
-                requestJSON.parties.push({
-                    name: "Democrats",
-                    nb_seats: parseInt(this.value),
-                    color: "#0000FF",
-                    border_size: 0,
-                    border_color: "#000000"
-                });
-                break;
-            case "Rep":
-                requestJSON.parties.push({
-                    name: "Republicans",
-                    nb_seats: parseInt(this.value),
-                    color: "#FF0000",
-                    border_size: 0,
-                    border_color: "#000000"
-                });
-                break;
-            case "Ind":
-                requestJSON.parties.push({
-                    name: "Independents",
-                    nb_seats: parseInt(this.value),
-                    color: "#C9C9C9",
-                    border_size: 0,
-                    border_color: "#000000"
-                });
-                break;
-            case "Vac":
-                requestJSON.parties.push({
-                    name: "Vacant",
-                    nb_seats: parseInt(this.value),
-                    color: "#6B6B6B",
-                    border_size: 0,
-                    border_color: "#000000"
-                });
-                break;
-        }
-    });
+    // This is what we send to the python script
+    const requestJSON = {
+        denser_rows: false, //Because I'm not going to implement this here yet
+        parties: [
+            {
+                name: "Democrats",
+                nb_seats: Math.max(0, parseInt($("#demNumber").val())),
+                color: "#0000FF",
+                border_size: 0,
+                border_color: "#000000",
+            },
+            {
+                name: "Republicans",
+                nb_seats: Math.max(0, parseInt($("#repNumber").val())),
+                color: "#FF0000",
+                border_size: 0,
+                border_color: "#000000",
+            },
+            {
+                name: "Independents",
+                nb_seats: Math.max(0, parseInt($("#indNumber").val())),
+                color: "#C9C9C9",
+                border_size: 0,
+                border_color: "#000000",
+            },
+            {
+                name: "Vacant",
+                nb_seats: Math.max(0, parseInt($("#vacNumber").val())),
+                color: "#6B6B6B",
+                border_size: 0,
+                border_color: "#000000",
+            },
+        ],
+    };
+
     console.log(requestJSON);
 
     //Now post the request to the script which actually makes the diagram.
