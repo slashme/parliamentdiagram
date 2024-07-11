89e57ce36181a1496f53ee711a09d0b9a7764dc2
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-07-12 01:07:03 +0200

    Drop str implementation
    without support for <title>, it had no future

--- src/template.py
+++ src/template.py
@@ -4,11 +4,6 @@ import re
 import sys
 import xml.etree.ElementTree as ET
 
-# watch out for malformed templates and attacks
-# maybe make one with no xml parsing whatsoever, relying only on regex, replacing id="@\d*@\d+" with the fill, stroke and stroke-width.
-
-# TODO: replace invalid @0@0 ids with a0s0 ids
-
 # SeatData = TypedDict("SeatData", {"fill": str, "stroke": str, "stroke-width": str, "title": str}, total=False)
 # SeatData = dict[str, str]
 # Warning : all values must be str, no numbers allowed
@@ -31,7 +26,7 @@ for p, n in _NAMESPACES.items():
     ET.register_namespace(p, n)
 del p, n
 
-def main(template_file, output_file=sys.stdout, *, filling=None, use_ET: bool = True, use_classes: bool|None = None) -> int|str|None:
+def main(template_file, output_file=sys.stdout, *, filling=None, use_classes: bool|None = None) -> int|str|None:
     """
     Considers the template file and the output file to be a file descriptor or a file path.
 
@@ -40,54 +35,42 @@ def main(template_file, output_file=sys.stdout, *, filling=None, use_ET: bool = 
     where there is one dict per area
     and the sum of nseats is the number of seats in that area,
     then the template gets filled using that filling.
-    If filling is True, it is replaced by a rainbow filling.
+    If filling is True, it is replaced by a rainbow filling, in "demo mode".
 
-    use_ET decides whether a string filling is used (if False)
-    or an ElementTree filling (if True).
-    The string filling doesn't support the <title> insertion,
-    so it's not used by default,
-    however it may be more resilient against attacks.
-
-    use_classes only matters when use_ET is True.
-    If True, the style of each SeatData is written in a <style> entry
+    If use_classes is True, the style of each SeatData is written in a <style> entry
     with a class that is set to each seat.
     If False, the style is written separately on each seat.
-    The False case is only better when each seat has different SeatData.
-    The parameter defaults to True if and when filling is not True.
+    The False case is only better when each seat has different SeatData
+    (which is typically the case in demo mode).
+    The parameter defaults to the opposite of filling being True.
     """
+
     if isinstance(template_file, str):
         with open(template_file, "r") as file:
-            return main(file, output_file, filling=filling, use_ET=use_ET)
+            return main(file, output_file, filling=filling)
     if isinstance(output_file, str):
         with open(output_file, "w") as file:
-            return main(template_file, file, filling=filling, use_ET=use_ET)
+            return main(template_file, file, filling=filling)
 
     if use_classes is None:
         use_classes = filling is not True
 
     template_str = template_file.read()
-    if use_ET:
-        # template_ET = _parse_ET_without_namespaces(template_str)
-        template_ET = ET.fromstring(template_str)
+    # template_ET = _parse_ET_without_namespaces(template_str)
+    template_ET = ET.fromstring(template_str)
 
     if filling is True:
-        nseats_by_area = _scan_str_template(template_str)
+        nseats_by_area = _scan_ET_template(template_ET)
         filling = [{SeatData(fill=color): 1 for color in _generate_rainbow(nseats, 250)} for nseats in nseats_by_area]
 
     if filling is None:
-        if use_ET:
-            print(_scan_ET_template(template_ET), file=output_file)
-        else:
-            print(_scan_str_template(template_str), file=output_file)
+        print(_scan_ET_template(template_ET), file=output_file)
     else:
-        if use_ET:
-            if use_classes:
-                _fill_ET_template_by_class(template_ET, filling)
-            else:
-                _fill_ET_template(template_ET, filling)
-            print(ET.tostring(template_ET, encoding="unicode"), file=output_file)
+        if use_classes:
+            _fill_ET_template_by_class(template_ET, filling)
         else:
-            print(_fill_str_template(template_str, filling), file=output_file)
+            _fill_ET_template(template_ET, filling)
+        print(ET.tostring(template_ET, encoding="unicode"), file=output_file)
 
 def _parse_ET_without_namespaces(string) -> ET.Element:
     """
@@ -125,14 +108,6 @@ def _scan_ET_template(template: ET.Element) -> list[int]:
     _sorted_areas, l1_elements_by_area = _extract_ET_template(template, check_unicity=True)
     return list(map(len, l1_elements_by_area))
 
-def _scan_str_template(template: str) -> list[int]:
-    # part 1:
-    # identify the seats
-    # return the number of seats per area to the form generator
-    # (probably an ordered list of seat counts)
-    _sorted_areas, l1_elements_by_area = _extract_str_template(template, check_unicity=True)
-    return list(map(len, l1_elements_by_area))
-
 def _extract_ET_template(template: ET.Element, *, check_unicity=False):
     elements_by_area = defaultdict(dict[int, ET.Element])
     for node in template.findall(".//"): # check that it takes the subelements
@@ -142,19 +117,6 @@ def _extract_ET_template(template: ET.Element, *, check_unicity=False):
             if check_unicity and (seat in area):
                 raise Exception(f"Duplicate seat : {ma.group(0)!r}")
             area[seat] = node
-    sorted_areas = sorted(elements_by_area)
-    l1_elements_by_area = [elements_by_area[area] for area in sorted_areas]
-
-    return sorted_areas, l1_elements_by_area
-
-def _extract_str_template(template: str, *, check_unicity=False):
-    elements_by_area = defaultdict(set[int])
-    for ma in re.finditer(r'id="(?:@(\d+))?@(\d+)"', template):
-        area = elements_by_area[int(ma.group(1) or "0")]
-        seat = int(ma.group(2))
-        if check_unicity and (seat in area):
-            raise Exception(f"Duplicate seat : {ma.group(0)!r}")
-        area.add(seat)
     sorted_areas = sorted(elements_by_area)
     l1_elements_by_area = [elements_by_area[area] for area in sorted_areas]
 
@@ -243,37 +205,6 @@ def _fill_ET_template_by_class(template: ET.Element, filling: list[dict[SeatData
                     el = ET.Element(d)
                     el.text = v
                     node.append(el)
-
-def _fill_str_template(template: str, filling: list[dict[SeatData, int]]) -> str:
-    """
-    The operation is not done in-place since str is immutable.
-    title and desc are not properly managed (treated as attributes).
-    """
-
-    # extract the seat svg elements again
-    # group them by area
-    sorted_areas, l1_elements_by_area = _extract_str_template(template)
-
-    # sort each area's set of seats (by number, not by alphabet)
-    # converting from list of sets to list of sorted lists
-    l2_elements_by_area = list(map(sorted, l1_elements_by_area))
-
-    # for each area:
-    for area_id, elements, fillings in zip(sorted_areas, l2_elements_by_area, filling, strict=True):
-        fillings_iter = chain(*(repeat(sd, r) for sd, r in fillings.items()))
-        # for each seat element:
-        for element_id in elements:
-            try:
-                seat_data = next(fillings_iter)
-            except StopIteration:
-                raise ValueError(f"Area {area_id} has the wrong number of filling seat data")
-
-            # fill the seats progressively, by applying them the style properties and removing their id
-            template = template.replace(f'id="@{area_id}@{element_id}"', " ".join(f'{k}="{v}"' for k, v in seat_data.items()))
-            if not area_id:
-                template = template.replace(f'id="@{element_id}"', " ".join(f'{k}="{v}"' for k, v in seat_data.items()))
-
-    return template
 
 if __name__ == "__main__":
     sys.exit(main(*sys.argv[1:]))
